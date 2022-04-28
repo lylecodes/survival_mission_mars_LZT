b87a2e8c6e79bef7231a6c624197004f5b53b991
Kristen Cotten <kristen.y.cotten@gmail.com>
2022-04-28 12:59:46 -0500

    added splash, startGame, quitGame, newGame, basic info, and verb/noun parse, AB#200, AB#201, AB#202,AB#203, AB#204

--- src/com/mars/Engine.java
+++ src/com/mars/Engine.java
@@ -1,2 +1,43 @@
-package com.mars;class Engine {
-}
+package com.mars;
+
+import com.mars.display.Display;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Engine {
+Display display = new Display();
+TextParser parser = new TextParser();
+
+    public void runApp() {
+        display.displaySplash();                        //Display welcome screen to user
+
+        boolean isRunning = false;
+        String answer = display.playGame();             //Ask if user wants to play a game
+        if(answer.equals("Y")){
+            isRunning = true;
+        }
+        else{
+            System.out.println("You chose to not play :(");
+            System.exit(0);
+        }
+        //boolean choseQuit = false;
+        while (isRunning) {
+            display.displayGameInfo();                  //display basic game info
+            display.displayMenu();                      //display available game commands
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a command: ");
+            String userInput = scanner.nextLine();
+            List<String> nextCommand = parser.getCommand(userInput);
+
+
+            System.out.println("Do you want to exit the game?");
+            String userAnswer = scanner.nextLine();
+            if (userAnswer.equals("Y")) {
+                System.out.println("Exiting game...");
+                break;
+            }
+        }
+    }//end method runApp
+}//end class engine
