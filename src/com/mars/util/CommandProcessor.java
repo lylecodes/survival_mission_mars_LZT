c8fe1915de3b56dea3ec689f4a38ca41a0ec07b5
Jeffrey Lee <dr3adnought@protonmail.com>
2022-05-02 13:12:07 -0400

    added comments for CommandProcessor and Engine

--- src/com/mars/util/CommandProcessor.java
+++ src/com/mars/util/CommandProcessor.java
@@ -7,24 +7,24 @@ import java.util.Map;
 
 public class CommandProcessor {
 
-
+    // method to resolve action command inputs from user
     public String processCommand(List<String> command, Location currentLocation, Map<String, Location> locationMap) {
-        String nextLocation = currentLocation.getName();
-        if(command.get(0).equals("go")) {
-            if(currentLocation.getDirections().containsKey(command.get(1))) {
-                nextLocation = currentLocation.getDirections().get(command.get(1));
+        String nextLocation = currentLocation.getName();                                                                // getting name of currentLocation and assign to nextLocation
+        if(command.get(0).equals("go")) {                                                                               // checking if input command is 'go'
+            if(currentLocation.getDirections().containsKey(command.get(1))) {                                           // checking if currentLocation has direction of movement provided by user input as an option
+                nextLocation = currentLocation.getDirections().get(command.get(1));                                     // moving to nextLocation
             } else {
-                System.out.println("C'mon, get right, you can't go that way!");
+                System.out.println("C'mon, get right, you can't go that way!");                                         // otherwise informing user that pathway is not accessible
             }
         }
-        else if(command.get(0).equals("quit")) {
-            System.out.println("Fine then! Bye!!");
-            System.exit(0);
+        else if(command.get(0).equals("quit")) {                                                                        // checking if user inputs option to 'quit'
+            System.out.println("Fine then! Bye!!");                                                                     // sends quit message
+            System.exit(0);                                                                                      // exits game
         }
         else {
-            System.out.println("That is an invalid command. Please try again.");
+            System.out.println("That is an invalid command. Please try again.");                                        // if user input is not 'go' or 'quit', informs user of invalid command input
         }
-        return nextLocation;
+        return nextLocation;                                                                                            // returns new location if needed elsewhere
     }
 
 }
