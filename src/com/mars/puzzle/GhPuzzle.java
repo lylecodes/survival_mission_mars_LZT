5fc076072b50d3c24d59595fcb609bd18b3b1c48
Kristen Cotten <kristen.y.cotten@gmail.com>
2022-05-06 12:17:02 -0500

    added GhPuzzle class and GH Intro, AB#299

---
+++ src/com/mars/puzzle/GhPuzzle.java
@@ -0,0 +1,61 @@
+package com.mars.puzzle;
+
+import com.mars.display.Display;
+
+import java.util.Scanner;
+
+public class GhPuzzle implements Puzzle{
+    //fields
+    boolean isSolved = false;
+    Display display = new Display();
+
+    @Override
+    public void showIntro() {
+        display.displayText("text/greenHouseIntro.txt");
+    }
+
+    @Override
+    public void runPuzzle() {
+        greenHousePuzzle(); //run the room specific puzzle
+    }
+
+    @Override
+    public boolean isSolved() {
+        return isSolved;
+    }
+    private void greenHousePuzzle(){
+        //green house specific challenge to grow food
+//        Turn valve
+//        Water mister engages and moistens the soil
+//        Plant seeds
+        if(!isSolved){
+            showIntro();
+            System.out.print("You notice a valve in the corner of the room connected to " +
+                    "the water main. Do you turn it on? Enter 'y' or 'n'\n>> ");
+            Scanner scanner = new Scanner(System.in);
+            String turnValve = scanner.nextLine();
+            turnValve.toLowerCase();
+            if(turnValve.equals("y")){
+                System.out.println("You see the water mister engage and moisten the soil." +
+                        "Would you like to plant some seeds? Enter 'y' or 'n'\n>> ");
+                String plantSeeds = scanner.nextLine();
+                plantSeeds.toLowerCase();
+                if(plantSeeds.equals("y")){
+                    System.out.println("Congratulations! You are able to grow food on Mars!");
+                    isSolved = true;
+                }
+                else {
+                    System.out.println("Oh well, your loss...have fun eating your MREs...");
+                }
+            }
+            else{
+                System.out.println("Hmm....interesting choice.");
+            }
+
+        }
+        else{
+            System.out.println("It looks like you've solved this challenge already!");
+        }
+
+    }
+}
