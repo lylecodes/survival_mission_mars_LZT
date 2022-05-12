a6acb7b8fc67e4f3d615d65ecf06d14d33ccd41d
Wintersolid <ztanksley@luc.edu>
2022-05-12 15:11:42 -0500

    Game.java,GameFrame added

---
+++ src/main/java/com/mars/gui/alt/Game.java
@@ -0,0 +1,15 @@
+package com.mars.gui.alt;
+
+import java.awt.*;
+import javax.swing.*;
+
+class Game {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() ->
+        {
+            GameFrame gameFrame = new GameFrame();
+            gameFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            gameFrame.setVisible(true);
+        });
+    }
+}
