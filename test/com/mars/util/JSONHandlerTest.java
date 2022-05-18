b6ae34994e192b62b32092ea795841df11039cfb
Lyle Jensen <lyle.jensen95@gmail.com>
2022-05-18 12:15:03 -0600

    added more tests for jsonhandler AB#693

--- test/com/mars/util/JSONHandlerTest.java
+++ test/com/mars/util/JSONHandlerTest.java
@@ -1,14 +1,15 @@
 package com.mars.util;
 
+import com.mars.objects.Location;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
+import java.util.Map;
 
 public class JSONHandlerTest {
-
     JSONHandler jsonHandler;
     String txtFilePath = "text/game_info.txt";
 
@@ -21,5 +22,12 @@ public class JSONHandlerTest {
     public void getFileFromResourceAsStream_shouldReturnNonNullInputStream_whenGivenTextFilePath() {
         InputStream inputStream = JSONHandler.getFileFromResourceAsStream(txtFilePath);
         assertNotNull(inputStream);
+    }
+
+    @Test
+    public void loadLocationMap_shouldPopulateMapWithLocations_whenGivenJson() {
+        Map<String,Location> locationMap = jsonHandler.loadLocationMap();
+        assertNotNull(locationMap);
+        assertNotNull(locationMap.get("Hydro"));
     }
 }
